43267fe06cb9858948bc421d4b14f568d662b0fc
AllenLeeeeee <40710678+AllenLeeeeee@users.noreply.github.com>
2019-04-19 14:28:31 +0800

    Update demo.R

--- demo.R
+++ demo.R
@@ -24,6 +24,8 @@ D <- data.frame(x,group)
 x1 <- as.matrix(subset(D,group==1)[,-(p+1)])
 x2 <- as.matrix(subset(D,group==2)[,-(p+1)])
 x3 <- as.matrix(subset(D,group==3)[,-(p+1)])
+
+
 ######generate Y's#########
 y1 <- a1+x1%*%b1+rnorm(dim(x1)[1],mean = 0,sd=sd1)
 y2 <- a2+x2%*%b2+rnorm(dim(x2)[1],mean = 0,sd=sd2)
@@ -33,7 +35,18 @@ hist(frame$y)
 
 
 ################classification#################
-source('gmm_clust1d.R')
-clust_result <- gmm_clust1d(data = frame$y,k = 3,mu = c(-5,0,5),sigma2 = c(1,1,1),PI = c(1,1,1)/3,N = 20)
+source('gmm_clust1d.R') 
+clust_result <- gmm_clust1d(data = frame$y, k = 3,mu = c(-5,0,5),
+                            sigma2 = c(1,1,1),PI = c(1,1,1)/3,N = 20)
 tab <- table(frame$group,clust_result$clust.pred)
 TPM <- 1-sum(diag(tab))/sum(tab)
+
+
+###############dimension reduction#############
+G <- apply(data.frame(c(1:3)), 1, function(x){frame[which(clust_result$clust.pred==x),]})
+s1 <- dr(y~X1+X2+X3+X4,data = G[[1]])
+summary(s1)
+s2 <- dr(y~X1+X2+X3+X4,data = G[[2]])
+summary(s2)
+s3 <- dr(y~X1+X2+X3+X4,data = G[[3]])
+summary(s3)
